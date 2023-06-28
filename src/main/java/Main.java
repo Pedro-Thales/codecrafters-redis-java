72a919a799dabee7f0c786d56061254e2b29990f
Pedro Bastos <pedro_thales@hotmail.com>
2023-06-28 20:11:05 -0300

    pass second stage and maybe further

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
-
         //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
@@ -16,6 +16,35 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
+
+            while (true) {
+                String s = br.readLine();
+
+                if (s == null) {
+                    continue;
+                }
+
+                if (s.startsWith("*") || s.startsWith("$")) {
+                    continue;
+                }
+
+                switch (s.toLowerCase(Locale.ROOT)) {
+                    case "ping":
+                        pw.write("+PONG\r\n");
+                        pw.flush();
+                    case "exit":
+                        break;
+                    default:
+                        pw.write("+INVALID COMMAND\r\n");
+                        pw.flush();
+                }
+
+            }
+
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
